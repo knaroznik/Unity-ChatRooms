b6ab0c22ae1da96f5e9d962be0e9d9080a9da617
McCorner <McCorner@wp.pl>
2017-05-25 01:06:13 +0200

    Lobby Host Chat Room.

---
+++ Assets/Scripts/Lobby/LobbyCenter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LobbyCenter : MonoBehaviour {
+
+	private string p_chatName;
+	private uint p_roomSize = 6;
+	private NetworkManager p_networkManager;
+
+	void Start () {
+		p_networkManager = NetworkManager.singleton;
+		if (p_networkManager.matchMaker == null)
+		{
+			p_networkManager.StartMatchMaker();
+		}
+	}
+
+	public void SetChatName (string l_chatName)
+	{
+		p_chatName = l_chatName;
+	}
+
+	public void CreateRoom ()
+	{
+		if (p_chatName != "" && p_chatName != null) {
+			Debug.Log ("Creating Room: " + p_chatName + " with room for " + p_roomSize + " players.");
+			p_networkManager.matchMaker.CreateMatch (p_chatName, p_roomSize, true, "", "", "", 0, 0, p_networkManager.OnMatchCreate);
+		} else {
+			Debug.Log ("Need to pick a name");
+		}
+	}
+}
